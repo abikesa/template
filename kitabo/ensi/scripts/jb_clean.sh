04810a121f71ccbed77ff70bf48bd3599d85f0d6
abikesa <abikesa.sh@gmail.com>
2024-08-02 12:22:09 -0400

    text, context, pretext: art, language, transformers

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,3 +1,9 @@
 # transfer after every jb clean .
 mkdir -p _build/html/pdfs  
 cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
+cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
+cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
+cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
+mkdir -p _build/html/media
+cp media/jesus-loves-me.mp3 _build/html/media/jesus-loves-me.mp3
+cp -r act1/app/* _build/html/act1/app
