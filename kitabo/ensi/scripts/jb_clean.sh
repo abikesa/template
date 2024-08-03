48144ed9e724e1e2ac6f6423c2e5d7f6ddc3703a
abikesa <abikesa.sh@gmail.com>
2024-08-03 15:53:17 -0400

    figures and images, take5

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,7 +1,6 @@
 mkdir -p _build/html/pdfs _build/html/figures _build/html/media
 cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf} _build/html/pdfs/
 cp media/jesus-loves-me.mp3 _build/html/media/
-cp figures/bwv1068.png _build/html/figures/bwv1068.png
 cp -r act1/app/* _build/html/act1/app
 
 
