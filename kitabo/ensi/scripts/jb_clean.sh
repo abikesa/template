699cfd8cec0f26499e93a8749359a779fed73a5a
abikesa <abikesa.sh@gmail.com>
2024-08-03 14:16:03 -0400

    stream linked jb_clean.sh

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,9 +1,5 @@
-# transfer after every jb clean .
-mkdir -p _build/html/pdfs  
-cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
-cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
-cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
-cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
-mkdir -p _build/html/media
-cp media/jesus-loves-me.mp3 _build/html/media/jesus-loves-me.mp3
+mkdir -p _build/html/pdfs _build/html/images _build/html/media
+cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf} _build/html/pdfs/
+cp images/bwv1068ii.png _build/html/images/
+cp media/jesus-loves-me.mp3 _build/html/media/
 cp -r act1/app/* _build/html/act1/app
