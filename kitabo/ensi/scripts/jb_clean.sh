27abf083875049f062d0cb1cb44b0084d6d0d609
abikesa <abikesa.sh@gmail.com>
2024-08-03 14:17:59 -0400

    figures vs. images

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,5 +1,5 @@
 mkdir -p _build/html/pdfs _build/html/images _build/html/media
 cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf} _build/html/pdfs/
-cp images/bwv1068ii.png _build/html/images/
+cp figures/bwv1068ii.png _build/html/figures/
 cp media/jesus-loves-me.mp3 _build/html/media/
 cp -r act1/app/* _build/html/act1/app
