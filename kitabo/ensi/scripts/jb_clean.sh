e958007060aad30fe32965be8104cedb67d3f2c8
abikesa <abikesa.sh@gmail.com>
2024-08-03 15:46:14 -0400

    tidy jb_clean.sh

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,5 +1,7 @@
 mkdir -p _build/html/pdfs _build/html/figures _build/html/media
 cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf} _build/html/pdfs/
-cp figures/bwv1068ii.png _build/html/figures/
 cp media/jesus-loves-me.mp3 _build/html/media/
 cp -r act1/app/* _build/html/act1/app
+
+
+
