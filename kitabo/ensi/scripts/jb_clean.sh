734211985ea1a696afddb946868f70969d3c9bae
abikesa <abikesa.sh@gmail.com>
2024-08-04 07:20:00 -0400

    created pdf views under mozart

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,6 +1,5 @@
 mkdir -p _build/html/pdfs _build/html/figures _build/html/media
-# cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf,paulhenry.pdf} _build/html/pdfs/
-cp pdfs/* _build/html/pdfs/
+cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf,paulhenry.pdf} _build/html/pdfs/
 cp media/jesus-loves-me.mp3 _build/html/media/
 cp -r act1/app/* _build/html/act1/app
 
