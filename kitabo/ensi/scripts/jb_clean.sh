0715017b16985d647a3c81774775df8e8f227e5b
abikesa <abikesa.sh@gmail.com>
2024-08-04 07:32:39 -0400

    use cp -r dir/* to avoid drama

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -1,6 +1,6 @@
 mkdir -p _build/html/pdfs _build/html/figures _build/html/media
-cp pdfs/{locke.pdf,bach.pdf,handel.pdf,mozart.pdf,paulhenry.pdf} _build/html/pdfs/
-cp media/jesus-loves-me.mp3 _build/html/media/
+cp -r pdfs/* _build/html/pdfs/
+cp -r media/* _build/html/media/
 cp -r act1/app/* _build/html/act1/app
 
 
